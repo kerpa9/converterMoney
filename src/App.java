dfcbf709dadff2363d49dd51c3eabf28004ee8ec
Kevin Reyes <kerpa9@gmail.com>
2024-11-03 23:26:37 -0500

    Create menu

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import com.kevenreyes.converter.ArsToUsd;
 import com.kevenreyes.converter.BlrToUsd;
 import com.kevenreyes.converter.CopToUsd;
@@ -8,13 +10,13 @@ import com.kevenreyes.services.ConsultApi;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
         ArsToUsd arsToUsd = new ArsToUsd();
         UsdToArs usdToArs = new UsdToArs();
         BlrToUsd blrToUsd = new BlrToUsd();
         CopToUsd copToUsd = new CopToUsd();
         UsdToBlr usdToBlr = new UsdToBlr();
         UsdToCop usdToCop = new UsdToCop();
+        Scanner optionValue = new Scanner(System.in);
 
         // usdToArs.converterUsdToArs();
         // arsToUsd.converterArsToUsd();
@@ -22,6 +24,59 @@ public class App {
         // copToUsd.converterCopToUsd();
         // usdToBlr.converterUsdToBrl();
         // usdToCop.converterUsdToCop();
+
+        String data = """
+                        ********** CONVERTER MONEY  ************
+
+                    Welcome to the currency converter app. 
+
+                              1./ Dollar          -> Argentine peso
+                              2./ Argentine peso  -> Dollar
+                              3./ Dollar          -> Brazilian real
+                              4./ Brazilian real  -> Dollar
+                              5./ Dollar          -> Colombian peso
+                              6./ Colombian peso  -> Dollar
+                              7./ Close APP
+
+                    Please choose a valid option.
+                """;
+
+        System.out.println(data);
+
+        int option = optionValue.nextInt();
+        // while (option != 7) {
+
+        switch (option) {
+            case 1:
+                System.out.println("Hola");
+
+                break;
+            case 2:
+                System.out.println("Mundo");
+
+                break;
+            case 3:
+                System.out.println("Mundo");
+
+                break;
+            case 4:
+                System.out.println("Mundo");
+
+                break;
+            case 5:
+                System.out.println("Mundo");
+
+                break;
+            case 6:
+                System.out.println("Mundo");
+
+                break;
+            case 7:
+                System.out.println("Thank you for using this app, goodbye");
+
+                break;
+        }
+        // }
 
     }
 }
